3a15538613c6bc0f5e22d5cdf5d46da889c1be0c
Andre Praeiro <andrepreiro@gmail.com>
2018-07-08 19:35:00 -0400

    lecture #107 menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,11 @@
+import '../common/template/dependecies'
 import React from 'react'
+
+import Header from '../common/template/header'
+
 
 export default props => (
     <div className="wrapper">
-        <h1>App</h1>
+        <Header />
     </div>
 )
