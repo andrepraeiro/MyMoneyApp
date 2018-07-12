3e2f1d476919e8764539aa615d88823e6b64fb52
Andre Praeiro <andrepreiro@gmail.com>
2018-07-11 20:58:43 -0400

    lecture #131 tabs visibility part 1

--- frontend/src/common/tab/tabReducer.js
+++ frontend/src/common/tab/tabReducer.js
@@ -1,9 +1,11 @@
-const INITAL_STATE = { select: '' }
+const INITAL_STATE = { select: '', visible: {} }
 
 export default (state = INITAL_STATE, action) => {
     switch (action.type) {
         case 'TAB_SELECTED':
             return { ...state, select: action.payload }
+            case 'TAB_SHOWED':
+            return { ...state, visible: action.payload }
         default:
             return state
     }
