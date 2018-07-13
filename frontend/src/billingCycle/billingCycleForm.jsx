1ae12719573ea72075ecfc8da32c57220e9c1f8c
Andre Praeiro <andrepreiro@gmail.com>
2018-07-12 22:03:19 -0400

    lecture #138 billingCycleForm with redux-form (part 1)

---
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class BillinCycleForm extends Component {
+    render() {
+        return (
+            <form role="form">
+                <div className="box-body">
+                    <div className="box-footer">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
+
+export default BillinCycleForm
