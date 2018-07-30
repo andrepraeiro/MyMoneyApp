be21d716b10e216f80dfe766f24308603beb7a41
Andre Praeiro <andrepreiro@gmail.com>
2018-07-29 21:55:01 -0400

    lecture #152 add and clonning credits

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,33 +1,56 @@
 import React, { Component } from 'react'
-import { Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Field, arrayInsert } from 'redux-form'
 
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import { billingCycleForm } from './billingCycleConsts'
 
 class CreditList extends Component {
+    add(index, item = {}) {
+        if (!this.props.readOnly) {
+            this.props.arrayInsert(billingCycleForm, 'credits', index, item)
+        }
+    }
     renderRows() {
         const list = this.props.list || []
         return list.map((item, index) => (
             <tr key={index}>
-            <td>
-                <Field
-                    name={`credits[${index}].name`}
-                    component={Input}
-                    placeholder="Informe o nome"
-                    readOnly={this.props.readOnly}
-                />
-            </td>
-            <td>
-                <Field
-                    name={`credits[${index}].value`}
-                    component={Input}
-                    placeholder="Informe o valor"
-                    readOnly={this.props.readOnly}
-                />
-            </td>
-            <td />
-        </tr>
-        ))        
+                <td>
+                    <Field
+                        name={`credits[${index}].name`}
+                        component={Input}
+                        placeholder="Informe o nome"
+                        readOnly={this.props.readOnly}
+                    />
+                </td>
+                <td>
+                    <Field
+                        name={`credits[${index}].value`}
+                        component={Input}
+                        placeholder="Informe o valor"
+                        readOnly={this.props.readOnly}
+                    />
+                </td>
+                <td>
+                    <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={() => this.add(index + 1)}
+                    >
+                        <i className="fa fa-plus" />
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={() => this.add(index + 1, item)}
+                    >
+                        <i className="fa fa-clone" />
+                    </button>
+                </td>
+            </tr>
+        ))
     }
 
     render() {
@@ -40,7 +63,7 @@ class CreditList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
-                                <th>Ações</th>
+                                <th className="table-actions">Ações</th>
                             </tr>
                         </thead>
                         <tbody>{this.renderRows()}</tbody>
@@ -51,4 +74,8 @@ class CreditList extends Component {
     }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch)
+export default connect(
+    null,
+    mapDispatchToProps
+)(CreditList)
