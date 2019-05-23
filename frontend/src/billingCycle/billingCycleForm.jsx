40a6662989ab15b43325d23f679d67473032df47
andre praeiro <andrepraeiro@gmail.com>
2019-05-22 22:40:13 -0400

    class #166 integrate credit and debt sum

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -11,8 +11,18 @@ import ItemList from './itemList'
 import Summary from './summary'
 
 class BillingCycleForm extends Component {
+    
+    calculateSummary(){
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+    
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props
+        const { sumOfCredits, sumOfDebts} = this.calculateSummary()
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -44,7 +54,7 @@ class BillingCycleForm extends Component {
                         placeholder="Informe o ano"
                     />
 
-                    <Summary credit={1000} debt={100}/>
+                    <Summary credit={sumOfCredits} debt={sumOfDebts}/>
 
                     <ItemList
                         cols="12 6"
