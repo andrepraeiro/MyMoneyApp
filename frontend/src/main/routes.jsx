ccd54f7a08d01ebcc5638ff0a93da7a760015ca3
andre praeiro <andrepraeiro@gmail.com>
2019-05-23 22:47:20 -0400

    class #184 frontend authentication - authOrApp

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
 import Dashboard2 from '../dashboard2/dashboard2'
 import Dashboard from '../dashboard/dashboard'
 import billingCycle from '../billingCycle/billingCycle'
+import AuthOrApp from './authOrApp'
 
 export default props => (
    <Router history={hashHistory}>
-      <Route path="/" component={Dashboard} />
-      <Route path="dashboard2" component={Dashboard2} />
-      <Route path="billingCycles" component={billingCycle} />
+      <Route path="/" component={AuthOrApp} >
+         <IndexRoute component={Dashboard} />
+         <Route path="dashboard2" component={Dashboard2} />
+         <Route path="billingCycles" component={billingCycle} />
+      </Route>
       <Redirect from="*" to="/" />
    </Router>
 )
