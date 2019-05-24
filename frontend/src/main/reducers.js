54ae77a4c46f0ccce5d39bad3f28292454441a8b
andre praeiro <andrepraeiro@gmail.com>
2019-05-23 21:29:42 -0400

    class #180 - frontend authentication  part #2

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -5,13 +5,15 @@ import {reducer as ToastrReducer} from 'react-redux-toastr'
 import dashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: ToastrReducer
+    toastr: ToastrReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
